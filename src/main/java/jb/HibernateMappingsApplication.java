a5a5921a41aa3bd13a5fc735727b241474d15550
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2019-03-10 22:25:07 +0100

    Initial commit

---
+++ src/main/java/jb/HibernateMappingsApplication.java
@@ -0,0 +1,13 @@
+package jb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateMappingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateMappingsApplication.class, args);
+    }
+
+}
