c320f2fd07fb3bbeac1591eee2163731ba9824d6
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2020-07-13 02:57:03 +0200

    Add args resolver - join

--- src/main/java/jb/rest/BookRest.java
+++ src/main/java/jb/rest/BookRest.java
@@ -3,9 +3,9 @@ package jb.rest;
 import jb.model.Book;
 import jb.service.BookService;
 import lombok.RequiredArgsConstructor;
-import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.GreaterThan;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
+import net.kaczmarzyk.spring.data.jpa.web.annotation.Join;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Or;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.domain.Page;
@@ -25,10 +25,20 @@ public class BookRest {
             @Or({
                     @Spec(path = "title", spec = Like.class),
                     @Spec(path = "year", spec = GreaterThan.class)
-            }) Specification<Book> bookSpecification//,
-//            Pageable pageable
+            }) Specification<Book> bookSpecification,
+            Pageable pageable
     ) {
-        return bookService.getBooks(bookSpecification, Pageable.unpaged());
+        return bookService.getBooks(bookSpecification, pageable);
+    }
+
+    @GetMapping("/booksByAuthor")
+    public Page<Book> getBooksByAuthor(
+            @Join(path = "authors", alias = "a")
+            @Spec(path = "a.firstName", params = "firstName", spec = Like.class)
+                    Specification<Book> bookSpecification,
+            Pageable pageable
+    ) {
+        return bookService.getBooks(bookSpecification, pageable);
     }
 
 }
